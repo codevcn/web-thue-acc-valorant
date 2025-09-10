f00c90c8b6b06f4d88e8e159a97686bc62496718
codevcn <codevoicainay@gmail.com>
2025-09-10 23:39:19 +0700

    update head files for SEO

--- src/views/home/page.php
+++ src/views/home/page.php
@@ -5,7 +5,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php require_once __DIR__ . '/../templates/head.php'; ?>
   <link rel="stylesheet" href="/pages/home/page.css">
-  <title>Thuê Acc Valorant - Dương Anh Tuấn</title>
+  <title>Thuê Acc Valorant Rank Cao - An Toàn, Nhanh Chóng | shopthuevalorantime.com</title>
+  <meta name="description" content="Thuê acc Valorant rank cao, nhiều skin tại shopthuevalorantime.com. Hỗ trợ thuê theo giờ, ngày, tuần. Đảm bảo bảo mật và chất lượng.">
+  <meta name="keywords" content="thuê acc valorant, thuê tài khoản valorant, thuê nick valorant, acc valo cho thuê, acc valorant theo giờ, thuê acc giá rẻ, thuê acc rank cao, thuê valorant">
+  <meta name="robots" content="index, follow">
+  <link rel="canonical" href="https://shopthuevalorantime.com/thueacc">
+
+
+  <meta property="og:title" content="Thuê Acc Valorant Giá Rẻ, Premium | Acc Rank, Skin Đẹp & Hỗ Trợ Nhanh Chóng">
+  <meta property="og:description" content="Thuê acc Valorant chất lượng cao tại ShopThuêValorantime.com. Đa dạng acc với mọi rank, skin súng và nhân vật đẹp. Dịch vụ uy tín, giá hợp lý, hỗ trợ 7h - 00h hàng ngày.">
+  <meta property="og:image" content="https://shopthuevalorantime.com/images/UI/bg-render.png">
+  <meta property="og:url" content="https://shopthuevalorantime.com/thueacc">
+  <meta property="og:type" content="website">
 </head>
 
 <body class="bg-gradient-to-br from-regular-from-blue-cl via-regular-via-blue-cl to-regular-to-blue-cl text-[14px] min-[1366px]:text-[16px] min-[1680px]:text-[18px] min-[1800px]:text-[20px] min-[2200px]:text-[24px]">
--- src/views/templates/head.php
+++ src/views/templates/head.php
@@ -3,3 +3,5 @@
 
 <link rel="stylesheet" href="/utils/styles/styles.css" />
 <link rel="stylesheet" href="/utils/styles/globals.css" />
+
+<script src="https://analytics.ahrefs.com/analytics.js" data-key="N9lq3j+2ZQTJUzeN7kNCAA" async></script>
